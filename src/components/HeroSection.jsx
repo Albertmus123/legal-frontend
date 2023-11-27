0974cc83c09359eddedd166acc727cb77feb0c98
i-lucie <nishimwelucie5@gmail.com>
2023-11-27 12:05:22 +0200

    phone nav

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -33,7 +33,7 @@ const HeroSection = () => {
   return (
     <motion.div className='heroImageb bg-cover bg-no-repeat bg-center grid justify-center pt-4 md:pt-16 lg:pt-24 pb-28'>
         <div className='hero-title py-12'>
-          <h1 className='text-gray-100 text-[40px] md:text-[60px] lg:text-[60px] ps-16 md:ps-32 lg:ps-48 pb-12'>MURAHO <span className='text-[#003566] bg-gray-100 rounded-xl px-2'>NEZA?</span> </h1>
+          <h1 className='text-gray-100 text-[40px] md:text-[60px] lg:text-[60px] ps-16 md:ps-32 lg:ps-48 pb-12'>MURAKAZA <span className='text-[#003566] bg-gray-100 rounded-xl px-2'>NEZA?</span> </h1>
           <p className='text-gray-100 text-[22px] md:text-[26px] lg:text-[26px] ps-6 md:p-0 lg:p-0 font-bold'> Tugishe Inama Mubyamategeko,  Turaguha Umu avoka uguhagararira, 
             <span className='block text-[#003566] text-[16px] md:text-[26px] lg:text-[26px] bg-gray-100 rounded-full me-4 ps-4 mg:ps-12 lg:ps-12 my-4'>Kurikirana ikirego cyawe aho kigeze .</span>
             
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import { TiThMenuOutline } from "react-icons/ti";
-import { CiSearch } from "react-icons/ci";
+import React, { useState } from 'react'
 import { GoLaw } from "react-icons/go";
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { SlSocialFacebook } from "react-icons/sl";
 import { FiTwitter } from "react-icons/fi";
 import { FaInstagram } from "react-icons/fa";
-import { CiLinkedin } from "react-icons/ci";
-
-
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+import { MdClose } from "react-icons/md";
 const listOfNavigation = [
     {
         link: 'Ahabanza',
@@ -30,8 +27,49 @@ const listOfNavigation = [
     }
 ]
 
+const NavbarPhoneVariants = {
+    hidden:{ y: '-100'},
+    visible: {
+         y: 0,
+         transition:{ duration:0.4}
+    },
+    exit :{
+        y: -300,
+        transition:{ duration:0.6}
+    }
+}
+
+const NavBarPhone = ()=>{
+    return (
+            <motion.nav 
+            variants={NavbarPhoneVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            className="fixed max-w-12 z-10 text-gray-100 bg-[#003566] mx-2 md:mx-28 lg:mx-24 rounded-md">
+            <ul
+            className='px-8 py-8 gap-6 md:gap-6 lg:gap-8'>
+                    {listOfNavigation.map((item, index) => {
+                        return (
+                            <motion.li
+                                initial={{ scale:1 }}
+                                whileHover={{ scale:1.1}}
+                                transition={{ duration: 0.2}}
+                                key={index}
+                                >
+                                <Link to={item.to} className='text-[13px] font-bold hover:border-b-4 hover:border-b-[#8338ec]'>{item.link}</Link>
+                            </motion.li>
+                        )
+                    })}
+                </ul>
+            </motion.nav>
+    )
+}
+
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false)
   return (
+    <>
     <div className='nav relative gap-12 grid justify-center shadow py-4  sticky top-0 right-0 z-30 text-gray-100  rounded-md bg-[#003566] mx-2 md:mx-28 lg:mx-24'>
         <div 
             className="logo absolute left-4 top-2 text-[40px]">
@@ -59,13 +97,21 @@ const NavBar = () => {
         <li><SlSocialFacebook /></li>
         <li><FiTwitter /></li>
         <li><FaInstagram /></li>
-        <li><CiLinkedin /></li>
+        {/* <li><CiLinkedin /></li> */}
       </ul>
         </div>
-        <div className="menu-icon absolute right-4 top-4 text-[30px] text-gray-100 show md:show lg:hidden">
-            <TiThMenuOutline />
+        <div
+        style={{cursor: 'pointer'}}
+        className="menu-icon absolute right-4 top-4 text-[35px] text-gray-100 show md:show lg:hidden ">
+        
+        {isOpen ? <MdClose onClick={()=>setIsOpen(!isOpen)}  />  : <HiOutlineBars3BottomRight onClick={()=>setIsOpen(!isOpen)} />}
+        
         </div>
     </div>
+    <AnimatePresence>
+    {isOpen && <NavBarPhone /> }
+    </AnimatePresence>
+    </>
   )
 }
 
